5e9c3c6a83100d28f5829efc2481354f4dba2b8c
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-04-06 19:32:58 -0400

    [NABI-5] set state

--- src/ts/Registration/components/Registration.tsx
+++ src/ts/Registration/components/Registration.tsx
@@ -2,7 +2,35 @@ import * as React from 'react';
 import Typography from 'material-ui/Typography';
 import RegistrationForm from './RegistrationForm';
 
-class Registration extends React.Component<{}, {}> {
+interface RegistrationProps {
+
+}
+
+interface RegistrationState {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  zipCode: string;
+  role: string;
+  hearAboutUs: string;
+}
+
+class Registration extends React.Component<RegistrationProps, RegistrationState> {
+  constructor(props: RegistrationProps) {
+    super(props);
+
+    this.state = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      zipCode: '',
+      role: 'student',
+      hearAboutUs: ''
+    };
+  }
+
   public render (): JSX.Element {
     return (
       <div className="nabi-container">
