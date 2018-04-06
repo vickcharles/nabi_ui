204b4f929557bfcdfae58101b121c068c0e17ca2
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-04-06 15:58:13 -0400

    [NABI-5] added montserrat font to page

--- src/ts/index.tsx
+++ src/ts/index.tsx
@@ -3,9 +3,39 @@ import * as ReactDOM from 'react-dom';
 import AppContainer from './main/components/AppContainer';
 import registerServiceWorker from '../registerServiceWorker';
 import '../assets/css/index.css';
+import createMuiTheme from 'material-ui/styles/createMuiTheme';
+import { MuiThemeProvider } from 'material-ui/styles';
+
+const theme = createMuiTheme({
+    typography: {
+      fontFamily: '\'Montserrat\', sans-serif !important'
+    }
+  });
+
+// export default function createMyTheme(options: ThemeOptions) {
+//   return createMuiTheme({
+//     typography: {
+//       fontFamily: '\'Montserrat\', sans-serif !important'
+//     },
+//     ...options,
+//   });
+// }
+
+// const theme = createMyTheme({ appDrawer: { breakpoint: 'md' }});
+// const theme = createMuiTheme({
+//   overrides: {
+//     MuiTypography: {
+//       colorPrimary: {
+//         color: '#06c3e1'
+//       }
+//     }
+//   }
+// });
 
 ReactDOM.render(
-  <AppContainer />,
+  <MuiThemeProvider theme={theme}>
+    <AppContainer />
+  </MuiThemeProvider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
