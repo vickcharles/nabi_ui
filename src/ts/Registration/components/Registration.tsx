589c14ad1050f1b924b8d100815054410029d5d3
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-04-06 19:26:43 -0400

    [NABI-5] turned registration into class

--- src/ts/Registration/components/Registration.tsx
+++ src/ts/Registration/components/Registration.tsx
@@ -2,17 +2,19 @@ import * as React from 'react';
 import Typography from 'material-ui/Typography';
 import RegistrationForm from './RegistrationForm';
 
-const Registration = () => {
-  return (
-    <div className="nabi-container">
-      <Typography className="nabi-page-title">
-        REGISTRATION
-      </Typography>
-      <div className="nabi-background-white nabi-section ">
-        <RegistrationForm/>
+class Registration extends React.Component<{}, {}> {
+  public render (): JSX.Element {
+    return (
+      <div className="nabi-container">
+        <Typography className="nabi-page-title">
+          REGISTRATION
+        </Typography>
+        <div className="nabi-background-white nabi-section ">
+          <RegistrationForm/>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Registration;
