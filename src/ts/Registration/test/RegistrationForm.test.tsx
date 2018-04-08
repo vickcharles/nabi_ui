f631320abd95cb69bf78b11a99d08da07e248937
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-04-08 10:58:16 -0400

    [NABI-5] finished unit tests for RegistrationForm

--- src/ts/Registration/test/RegistrationForm.test.tsx
+++ src/ts/Registration/test/RegistrationForm.test.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import RegistrationForm from '../components/RegistrationForm';
 import { mount, shallow } from 'enzyme';
+import { Role } from '../model';
+// import Select from 'material-ui/Select';
 
 describe('RegistrationForm', () => {
   let wrapper: any;
   const handleChange: () => void = jest.fn();
   const handleSubmit: () => void = jest.fn();
-
+  
   describe('Mount', () => {
     beforeEach(() => {
       wrapper = mount(
@@ -17,6 +19,63 @@ describe('RegistrationForm', () => {
           selectedRole=""
         />
       );
+    });
+
+    describe('When selectedRole is set to ' + Role.student, () => {
+      beforeEach(() => {
+        wrapper.setProps({ selectedRole: Role.student });
+      });
+
+      it('Text contains \'Register as a student\'', () => {
+        expect(
+          wrapper
+            .find('aside')
+            .at(0)
+            .text()
+        ).toEqual('Register as a student');
+      });
+
+      it('Contains two <Select />', () => {
+        expect(wrapper.find('fieldset')).toHaveLength(2);
+      });
+    });
+
+    describe('When selectedRole is set to ' + Role.parent, () => {
+      beforeEach(() => {
+        wrapper.setProps({ selectedRole: Role.parent });
+      });
+
+      it('Text contains \'Register as a parent\'', () => {
+        expect(
+          wrapper
+            .find('aside')
+            .at(0)
+            .text()
+        ).toEqual('Register as a parent');
+      });
+
+      it('Contains two <Select />', () => {
+        expect(wrapper.find('fieldset')).toHaveLength(2);
+      });
+    });
+    
+    describe('When selectedRole is set to ' + Role.instructor, () => {
+      beforeEach(() => {
+        wrapper.setProps({ selectedRole: Role.instructor });
+      });
+
+      it('Text contains \'Register as an instructor\'', () => {
+        expect(
+          wrapper
+            .find('aside')
+            .at(0)
+            .text()
+        ).toEqual('Register as an instructor');
+      });
+
+      it('Contains two <Select />', () => {
+        expect(wrapper.find('fieldset')).toHaveLength(1);
+      });
     });
   });
 
