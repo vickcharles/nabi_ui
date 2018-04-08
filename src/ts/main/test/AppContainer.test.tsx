be9fecceeb5ebbee7d47474619d241b7158b77b4
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-04-08 15:27:37 -0400

    [NABI-4] wrapped component with MemoryRouter

--- src/ts/main/test/AppContainer.test.tsx
+++ src/ts/main/test/AppContainer.test.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import AppContainer from '../components/AppContainer';
+import { MemoryRouter } from 'react-router-dom';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<AppContainer />, div);
+  ReactDOM.render(<MemoryRouter><AppContainer /></MemoryRouter>, div);
 });
