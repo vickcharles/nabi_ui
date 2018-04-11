569e0618d67a158cd2a8c12f0e490a896eed0d67
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-04-11 16:03:01 -0400

    [NABI-9] added stepper to profile builder

--- src/ts/ProfileBuilder/components/ProfileBuilder.tsx
+++ src/ts/ProfileBuilder/components/ProfileBuilder.tsx
@@ -1,9 +1,20 @@
 import * as React from 'react';
 import { Theme, withStyles, WithStyles } from 'material-ui/styles';
 import Typography from 'material-ui/Typography/Typography';
+import Stepper, { Step, StepButton } from 'material-ui/Stepper';
+import Button from 'material-ui/Button';
 
 const styles = (theme: Theme) => ({
-
+  button: {
+    marginRight: theme.spacing.unit,
+  },
+  completed: {
+    display: 'inline-block',
+  },
+  instructions: {
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit,
+  },
 });
 
 interface ProfileBuilderProps {
@@ -11,20 +22,178 @@ interface ProfileBuilderProps {
   handleSubmit: (event: React.FormEvent<{}>) => void;
   hearAboutUs: string;
   selectedRole: string;
+  classes: any;
 }
 
-type PropsWithStyles = ProfileBuilderProps & WithStyles<''>;
+// type PropsWithStyles = ProfileBuilderProps & WithStyles<''>;
 
-export class ProfileBuilder extends React.Component<PropsWithStyles> { 
-  public render(): JSX.Element { 
-      return (
-        <div className="nabi-container">
-          <Typography variant="title" className="nabi-margin-top-medium nabi-margin-bottom-medium">
-            PROFILE BUILDER
-          </Typography>
-        </div>
-      );
-    }
+interface ProfileBuilderState {
+  activeStep: number;
+  completed: any;
+}
+
+function getSteps() {
+  return ['Basic Info', 'Education', 'Experience', 'Qualifications', 'Recommendations'];
+}
+
+function getStepContent(stepIndex: any) {
+  switch (stepIndex) {
+    case 0:
+      return 'Basic Info';
+    case 1:
+      return 'Education';
+    case 2:
+      return 'Experience';
+    case 3:
+      return 'Qualifications';
+    case 4:
+      return 'Recommendations';
+    default:
+      return 'sd';
   }
+}
+
+export class ProfileBuilder extends React.Component
+<ProfileBuilderProps & WithStyles<'button' | 'completed' | 'instructions'>, ProfileBuilderState> { 
+  constructor(props: ProfileBuilderProps ) {
+    super(props);
+
+    this.state = {
+      activeStep: 0,
+      completed: {}
+    };
+  }
+
+  completedSteps() {
+    return Object.keys(this.state.completed).length;
+  }
+
+  totalSteps = () => {
+    return getSteps().length;
+  }
+
+  isLastStep() {
+    return this.state.activeStep === this.totalSteps() - 1;
+  }
+
+  allStepsCompleted() {
+    return this.completedSteps() === this.totalSteps();
+  }
+
+  handleNext = () => {
+    let activeStep;
+
+    if (this.isLastStep() && !this.allStepsCompleted()) {
+      // It's the last step, but not all steps have been completed,
+      // find the first step that has been completed
+      const steps = getSteps();
+      activeStep = steps.findIndex((step, i) => !(i in this.state.completed));
+    } else {
+      activeStep = this.state.activeStep + 1;
+    }
+    this.setState({
+      activeStep,
+    });
+  }
+
+  handleBack = () => {
+    const { activeStep } = this.state;
+    this.setState({
+      activeStep: activeStep - 1,
+    });
+  }
+
+  handleStep = (step: any ) => () => {
+    this.setState({
+      activeStep: step,
+    });
+  }
+
+  handleComplete = () => {
+    const { completed } = this.state;
+    completed[this.state.activeStep] = true;
+    this.setState({
+      completed,
+    });
+    this.handleNext();
+  }
+
+  handleReset = () => {
+    this.setState({
+      activeStep: 0,
+      completed: {},
+    });
+  }
+
+  public render(): JSX.Element { 
+    const { classes } = this.props;
+    const steps = getSteps();
+    const { activeStep } = this.state;
+   
+    return (
+      <div className="nabi-container">
+        <Typography variant="title" className="nabi-margin-top-medium nabi-margin-bottom-medium">
+          PROFILE BUILDER
+        </Typography>
+      
+        <Stepper nonLinear={true} activeStep={activeStep}>
+          {steps.map((label, index) => {
+            return (
+              <Step key={label}>
+                <StepButton
+                  onClick={this.handleStep(index)}
+                  completed={this.state.completed[index]}
+                >
+                  {label}
+                </StepButton>
+              </Step>
+            );
+          })}
+        </Stepper>
+        <div>
+          {this.allStepsCompleted() ? (
+            <div>
+              <Typography className={classes.instructions}>
+                All steps completed - you&quot;re finished
+              </Typography>
+              <Button onClick={this.handleReset}>Reset</Button>
+            </div>
+          ) : (
+            <div>
+              <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
+              <div>
+                <Button
+                  disabled={activeStep === 0}
+                  onClick={this.handleBack}
+                  className={classes.button}
+                >
+                  Back
+                </Button>
+                <Button
+                  variant="raised"
+                  color="primary"
+                  onClick={this.handleNext}
+                  className={classes.button}
+                >
+                  Next
+                </Button>
+                {activeStep !== steps.length &&
+                  (this.state.completed[this.state.activeStep] ? (
+                    <Typography variant="caption" className={classes.completed}>
+                      Step {activeStep + 1} already completed
+                    </Typography>
+                  ) : (
+                    <Button variant="raised" color="primary" onClick={this.handleComplete}>
+                      {this.completedSteps() === this.totalSteps() - 1 ? 'Finish' : 'Complete Step'}
+                    </Button>
+                  ))}
+              </div>
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default withStyles(styles)<ProfileBuilderProps>(ProfileBuilder);
