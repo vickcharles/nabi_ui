586080586091160ff5a39c2e9e2a417f61c9874f
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-04-13 12:49:36 -0400

    [NABI-15] added generateId method to registration and id to UserState model

---
+++ src/ts/ProfileBuilder/components/BasicInfo/NameAndLocation.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Typography from 'material-ui/Typography';
+
+const NameAndLocation = () => {
+    return (
+        <Typography variant="title" className="nabi-margin-top-medium nabi-margin-bottom-medium">
+            Basic Info
+        </Typography>
+    );
+};
+
+export default NameAndLocation;
