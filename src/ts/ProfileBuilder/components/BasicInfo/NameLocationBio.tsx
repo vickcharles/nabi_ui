094976bbe3d773c27d05d1cef4969c5f85d203d8
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-04-15 16:53:38 -0400

    [NABI-30] added bio textfield to NameLoactionBio

--- src/ts/ProfileBuilder/components/BasicInfo/NameLocationBio.tsx
+++ src/ts/ProfileBuilder/components/BasicInfo/NameLocationBio.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Typography from 'material-ui/Typography';
+import TextField from 'material-ui/TextField';
 
 interface NameLocationBioProps {
     firstName: string;
@@ -16,6 +17,22 @@ const NameLocationBio: React.StatelessComponent<NameLocationBioProps> = props =>
       <Typography className="nabi-text-center nabi-margin-bottom-medium">
         {props.zipCode}
       </Typography>
+      <TextField
+        id="bio"
+        margin="normal"
+        name="bio"
+        label="Bio"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        // TODO: add onChange
+        // onChange={handleChange}
+        placeholder="Bio"
+        required={true}
+        multiline={true}
+        fullWidth={true}
+        rows={9}
+      />
     </div>
   );
 };
