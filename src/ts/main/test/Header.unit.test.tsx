760e09c63fe2c9585cca69a2eac71182d9845ded
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-05-04 10:31:44 -0400

    [NABI-6] added logo, styled header and aadded header unit tests

---
+++ src/ts/main/test/Header.unit.test.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Header from '../components/Header';
+import { MemoryRouter } from 'react-router-dom';
+import { shallow } from 'enzyme';
+
+describe('Header', () => {
+  let wrapper: any = null;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    ).find(Header).dive();
+  });
+
+  it('Renders the component', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
