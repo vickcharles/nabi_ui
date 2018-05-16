9c32d99ad833c089e7b9d6aa75a4d4c7aea4fc8b
vikler charles <charlesvikler@gmail.com>
2018-05-16 15:01:47 -0500

    [NABI-110] background section

--- src/ts/main/components/Homepage.tsx
+++ src/ts/main/components/Homepage.tsx
@@ -1,12 +1,14 @@
 import * as React from 'react';
 import  { Link } from 'react-router-dom';
 import PageTitle from './PageTitle';
+import BackgroundImageSection from './BackgroundImageSection';
 
 const Homepage = () => {
     return (
         <div>
             <PageTitle pageTitle="HOMEPAGE" />
             <Link to="/registration">Registration</Link>
+             <BackgroundImageSection />
         </div>
     );
 };
