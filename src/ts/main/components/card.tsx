cc51e4710f6f0837ef5249c99cee940c4fd3f1b8
vikler charles <charlesvikler@gmail.com>
2018-05-19 13:40:09 -0500

    [NABI-117] Add card component

--- src/ts/main/components/card.tsx
+++ src/ts/main/components/card.tsx
@@ -3,12 +3,16 @@ import  Card  from 'material-ui/Card';
 import { CardContent } from 'material-ui/Card';
 import CardMedia from 'material-ui/Card';
 import Typography from 'material-ui/Typography';
+<<<<<<< HEAD
 import Button from 'material-ui/Button';
+=======
+>>>>>>> 88184c1... [NABI-117] Add card component
 
 /** 
  * Card component
  */
 
+<<<<<<< HEAD
 export interface CardProps {
   url: string;
   text: string;
@@ -35,6 +39,27 @@ const PopularInstrumentCard: React.StatelessComponent<CardProps> = props =>  {
         </CardContent>
         </Card>
       </div>
+=======
+const PopularInstrumentCard = () => {
+    return (
+        <div>
+        <Card>
+        <CardMedia>
+        <img src="http://poze.zoopedia.ro/poze/large/pasari/Wallpaper_Pasari_cinteza_aurie_americana.jpg" alt="" />
+        </CardMedia>
+          <CardContent>
+          <Typography  variant="headline" component="h2">
+            Lizard
+          </Typography>
+          <Typography component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+       
+      </Card>
+        </div>
+>>>>>>> 88184c1... [NABI-117] Add card component
     );
 };
 
