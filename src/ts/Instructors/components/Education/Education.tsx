7d0171271d066e4021a0b7be09d646cc1f78027c
Vanessa Charles Thompsonm <vane.charles24@gmail.com>
2018-05-25 09:45:31 -0400

    [NABI-136] added docs and ignored docs in gitignore

--- src/ts/Instructors/components/Education/Education.tsx
+++ src/ts/Instructors/components/Education/Education.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
 import Typography from 'material-ui/Typography';
 import Divider from 'material-ui/Divider';
-// import { LessonDuration, RatesState } from '../../model';
 
 import EducationFields from './EducationFields';
 
+/**
+ * Props for Education
+ * @interface EducationProps
+ */
 interface EducationProps {
 
 }
 
+/**
+ * Education: Second step on the Profile Builder
+ * @extends React.Component<EducationProps>
+ */
 const Education: React.StatelessComponent<EducationProps> = props => {
   return (
     <div>
