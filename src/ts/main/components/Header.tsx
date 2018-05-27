6084798b0ce64597d5c334a9c74d58bedd78a3f3
vikler charles <charlesvikler@gmail.com>
2018-05-27 13:22:24 -0500

    [NABI-65] added drawer menu and action buttons to header

--- src/ts/main/components/Header.tsx
+++ src/ts/main/components/Header.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+import DrawerMenu from './Drawer';
+import Button from 'material-ui/Button';
+import Divider from 'material-ui/Divider';
 
 /** 
  * Header component
@@ -7,21 +10,35 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
     <header>
-      <Link id="nabi-logo-anchor" to="/">
-        <img 
-          id="nabi-logo" 
-          className="nabi-text-center" 
-          alt="logo" 
-          src={require('../../../assets/images/logo.png')}
-        />
-        <p 
-          id="nabi-logo-text" 
-          className="nabi-text-center nabi-font-montserrat nabi-text-extrabold"
-        >
-          Nabi Music
-        </p>
-      </Link>
-    </header>
+      <div className="nabi-header-container">
+        <DrawerMenu />
+        <Divider className="header-divider hide-on-desktop"/>
+        <Link id="nabi-logo-anchor" to="/">
+          <img 
+            id="nabi-logo" 
+            className="nabi-text-center" 
+            alt="logo" 
+            src={require('../../../assets/images/logo.png')}
+          />
+          <p 
+            id="nabi-logo-text" 
+            className="nabi-text-center nabi-font-montserrat nabi-text-extrabold"
+          >
+            NABI MUSIC
+          </p>
+        </Link>
+        <div className="nabi-header-button" >
+          <Button className="nabi-responsive-button">
+          LOG IN
+          </Button>
+          <Link to="/registration">
+          <Button className="nabi-responsive-button" color="primary" variant="raised">
+          CREATE ACCOUNT
+          </Button>
+          </Link>
+        </div>
+      </div>
+   </header>
   );
 };
 
---
+++ src/ts/main/test/Drawer.unit.test.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import Drawer from '../components/Drawer';
+import { MemoryRouter } from 'react-router-dom';
+import { shallow } from 'enzyme';
+
+describe('Drawer', () => {
+  let wrapper: any = null;
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <MemoryRouter>
+        <Drawer />
+      </MemoryRouter>
+    ).find(Drawer).dive();
+  });
+
+  it('Renders the component', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+});
